99903ea3712aba94086ac8b8e0596e84077221e0
mting82 <156094524+mting82@users.noreply.github.com>
2024-03-17 11:16:22 +0800

    v1-17/3
    Layout for splash, login and register

--- app/src/main/java/utar/edu/project_management_app/MainActivity.java
+++ app/src/main/java/utar/edu/project_management_app/MainActivity.java
@@ -1,14 +1,22 @@
 package utar.edu.project_management_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
