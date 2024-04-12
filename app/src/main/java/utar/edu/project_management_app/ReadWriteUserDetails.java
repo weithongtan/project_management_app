b47eb18b2a7a02e8a0df221ec378d9e76b175ac1
mting82 <156094524+mting82@users.noreply.github.com>
2024-04-12 14:52:18 +0800

    database store email

--- app/src/main/java/utar/edu/project_management_app/ReadWriteUserDetails.java
+++ app/src/main/java/utar/edu/project_management_app/ReadWriteUserDetails.java
@@ -2,9 +2,11 @@ package utar.edu.project_management_app;
 
 public class ReadWriteUserDetails {
     public String username;
+    public String email;
 
     public ReadWriteUserDetails(){}
-    public ReadWriteUserDetails(String username){
+    public ReadWriteUserDetails(String username, String email){
         this.username = username;
+        this.email = email;
     }
 }
