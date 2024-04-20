3f7cdc5ad29922a523cdb33327b266c497df2dfa
elynt25 <elynteo25@1utar.my>
2024-04-20 11:35:13 +0800

    20/4
    arrange the code in project module

--- app/src/main/java/utar/edu/project_management_app/ProjectCreationBottomSheetDialogFragment.java
+++ app/src/main/java/utar/edu/project_management_app/ProjectCreationBottomSheetDialogFragment.java
@@ -1,19 +1,10 @@
 package utar.edu.project_management_app;
 import android.app.DatePickerDialog;
 import utar.edu.project_management_app.model.Project;
-import utar.edu.project_management_app.model.User;
-
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.icu.util.Calendar;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,40 +13,22 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ServerValue;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProjectCreationBottomSheetDialogFragment newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProjectCreationBottomSheetDialogFragment extends BottomSheetDialogFragment {
 
-    private TextView selectedDateTextView, listView, kanbanView, create_project_btn, cancel_project_btn;
+    private TextView selectedDateTextView, create_project_btn, cancel_project_btn;
     private EditText project_name;
-    private HashMap<String, Object> projects;
     private OnProjectCreatedListener projectCreatedListener;
 
-    String defaultView = "List"; //default view
-    // Get reference to the current user's projects node
-    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    String defaultView = "List";
     private int projectCount = 0;
     public interface OnProjectCreatedListener {
         void onProjectCreated(String projectId, String projectName);
@@ -122,10 +95,7 @@ public class ProjectCreationBottomSheetDialogFragment extends BottomSheetDialogF
                 }
 
                 String currentUserId = getCurrentUserId();
-
                 if (!proj_name.isEmpty() && !date.equals(defaultDateText)) {
-
-
                     addProjectToDB(proj_name, date, view);
                     dismiss();
 
@@ -141,15 +111,12 @@ public class ProjectCreationBottomSheetDialogFragment extends BottomSheetDialogF
                 dismiss();
             }
         });
-
         selectDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog();
             }
         });
-
-
         return view;
     }
 
@@ -157,36 +124,24 @@ public class ProjectCreationBottomSheetDialogFragment extends BottomSheetDialogF
 
         // Get the current user's ID
         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
-        // initialized project class
         Project newProject = new Project();
-
-
         // Get reference to the current user's projects node
         DatabaseReference currentUserProjectsRef = FirebaseDatabase.getInstance().getReference()
                 .child("Registered Users").child(userId).child("projects");
         // Push the project data under the user's projects node with the incremented project counter as the key
         DatabaseReference newProjectRef = currentUserProjectsRef.push();
         String projectId = newProjectRef.getKey();  // Get the generated project ID
-
         newProject.setProjectName(projName);
         newProject.setDueDate(date);
         newProject.setProjectId(projectId);
         newProject.getEmails().add(getCurrentUserEmail());
-
         // Store the project details under the generated project ID in the projects table
         DatabaseReference projectsRef = FirebaseDatabase.getInstance().getReference().child("projects").child(projectId);
         projectsRef.setValue(newProject);
-
         // Update the user's projectsId list
         DatabaseReference currentUserRef = FirebaseDatabase.getInstance().getReference()
                 .child("Registered Users").child(userId);
         currentUserRef.child("ProjectId").child(projectId).setValue(true); // Add project ID to the user's projectsId list
-
-        /*if (projectCreatedListener != null) {
-            projectCreatedListener.onProjectCreated(projectId, projName);
-        }*/
-
     }
 
     private String getCurrentUserId() {
@@ -205,31 +160,6 @@ public class ProjectCreationBottomSheetDialogFragment extends BottomSheetDialogF
         }
         return null;
     }
-
-    private boolean isFilled() {
-        boolean isProjectNameEmpty = project_name.getText().toString().isEmpty();
-        boolean isDateEmpty = !Character.isDigit(selectedDateTextView.getText().toString().charAt(0));
-
-
-        if (isProjectNameEmpty) {
-            project_name.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
-            project_name.setHintTextColor(Color.parseColor("#85FF0000"));
-            project_name.setError("Please fill out the project name");
-        } else {
-            project_name.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));// Reset to default or desired color when not empty
-
-        }
-
-        if (isDateEmpty) {
-            selectedDateTextView.setTextColor(Color.RED);
-            selectedDateTextView.setError("Please select date");
-        } else {
-            selectedDateTextView.setTextColor(Color.BLACK); // Reset to default or desired color when not empty
-        }
-
-        return !isProjectNameEmpty && !isDateEmpty;
-    }
-
     private void showDatePickerDialog() {
         // Get current date to set as default in DatePickerDialog
         final Calendar calendar = Calendar.getInstance();
